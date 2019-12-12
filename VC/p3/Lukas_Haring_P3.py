5c80006cd0c5e355c8a1803dbb5d974013b1bfea
Lukas Haring <lukasharing@hotmail.com>
2019-12-12 19:04:16 +0100

    Voice XML and VC P3

--- VC/p3/Lukas_Haring_P3.py
+++ VC/p3/Lukas_Haring_P3.py
@@ -10,6 +10,21 @@ os.path.dirname(os.path.abspath(__file__))
 def sigma2tam(sigma):
     return 2 * np.uint(sigma * 3) + 1;
 
+# Draw Keypoints because cv2 is ***
+def drawKeypoints(img, keypoints, color = (0, 0, 255)):
+    
+    result = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    
+    for keypoint in keypoints:
+        position = (int(round(keypoint.pt[0])), int(round(keypoint.pt[1])))
+        radius = int(round(keypoint.size / 2.))
+        
+        cv2.circle(result, position, radius, color, 1, cv2.LINE_AA)
+        direction = (position[0] + int(np.cos(keypoint.angle) * radius), position[1] + int(np.sin(keypoint.angle) * radius)) 
+        cv2.line(result, position, direction, color, 1, cv2.LINE_AA)
+        
+    return result
+    
 # Returns the image with a size of a power of two (the reminder is a padding of 0)
 def padding_image(img):
     
@@ -39,7 +54,8 @@ def gradient(img, sigma):
     grad_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize = ksize)
     grad_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize = ksize)
     
-    return list(zip(grad_x, grad_y))
+    # Pair each gradient x and gradient y
+    return np.dstack((grad_x, grad_y))
 
 # Returns next gaussian scale
 def gaussian_scale(img, sigma):
@@ -47,20 +63,32 @@ def gaussian_scale(img, sigma):
     gs_image = cv2.resize(gs_image, None, fx = 0.5, fy = 0.5, interpolation = cv2.INTER_NEAREST)
     return gs_image
 
-# Gaussian Scale of the harmonic means
-def harris_scales(img, num_scales, block_size):
-    harris_scales = []
-    # Paper constants
-    integration_scale = 1.5 # Gaussian coefficient
-    derivate_scale    = 1.0 # Derivate coefficient
+def gaussian_scales(img, num_scales):
     
     last_scale = img
+    scales = [img]
+    
+    # Paper constant
+    integration_scale = 1.5 # Gaussian coefficient
+    
     for i in range(num_scales):
-        scale_block = (i + 1) * block_size
+        last_scale = gaussian_scale(last_scale, integration_scale)
+        scales.append(last_scale)
+    
+    return scales
+
+# Gaussian Scale of the harmonic means
+def harris_scales(gaussian_scales):
+    harris_scales = []
+    # Paper constant
+    derivate_scale    = 1.0 # Derivate coefficient
+    
+    for i in range(len(gaussian_scales)):
+        scale_block = int(8 / np.power(2, i))
         
         # Eigen Values / Vectors of each pixel
         eigen_image = cv2.cornerEigenValsAndVecs(
-            last_scale,
+            gaussian_scales[i],
             blockSize = scale_block,
             ksize = sigma2tam(derivate_scale),
             borderType = cv2.BORDER_CONSTANT
@@ -71,54 +99,82 @@ def harris_scales(img, num_scales, block_size):
             eigen_image[:,:,0] * eigen_image[:,:,1] / (eigen_image[:,:,0] + eigen_image[:,:,1])
         )
         
-        last_scale = gaussian_scale(last_scale, integration_scale)
         
     return harris_scales
 
-def nonmaximun_scale_supression(scales):
+# Non-Maximum Supression and High Pass Supression
+def maximums_harris_scale(grad, scales, high_pass = 10.):
     if len(scales) <= 1: return scales
     
-    # Add Padding (1, 1) to all scales to suppress    
     # Non Maximum Supression For Each Scale
+    maximums = []
     for k in range(len(scales)):
+        scale_maximums = []
         scale = scales[k]
-        width, height = scales[k].shape
+        width, height = scale.shape        
+        # Factor Scales
+        scalef = np.power(2, k)
         for j in range(1, height):
             for i in range(1, width):
                 px = scale[j, i]
-                if px > 0:
-                    maximum = np.amax(scale[j - 1 : j + 2, i - 1 : i + 2])
-                    if px < maximum:
-                        scale[j, i] = 0
-    
-    return scales
+                if not(np.isnan(px)) and px >= high_pass:
+                    nmax = np.amax(scale[j - 1 : j + 2, i - 1 : i + 2])
+                    if px >= nmax:
+                        sx = i * scalef
+                        sy = j * scalef
+                        angle = np.arctan2(grad[sy][sx][1], grad[sy][sx][0])
+                        scale_maximums.append(cv2.KeyPoint(sx, sy, scalef * 16., angle))
+        maximums.append(scale_maximums)
+        
+    return maximums
+
 
 # Ejercicio 1 Gaussian Blur and Get Deriv Kernels
-def ejercicio1(img):
+def ejercicio1(img, high_pass = 10.):
     # Sizes image
     width, height = img.shape
     
     # Resize Image to a power of two
     rimg = padding_image(img)
-    
-    # Calculate Gradient
-    igrad = gradient(rimg, 4.5)
+
+    # Gradient and Gaussian Scales    
+    igrad = gradient(img, 4.5)
+    gaussians = gaussian_scales(rimg, 3)
     
     # Harris Gaussian Scales
-    scales = harris_scales(rimg, 2, 4)
-    
+    scales = harris_scales(gaussians)
+
     # Maximum Scale Supression
-    maximum = nonmaximun_scale_supression(scales)
+    maximums = maximums_harris_scale(igrad, scales, high_pass)
     
-    print(maximum[0])
-    # Unpadding
-    result = rimg[ :width, :height]
+    # Draw one next to another
+    result = np.zeros((height, width * len(gaussians), 3), dtype = rimg.dtype)
+    
+    keypoints = drawKeypoints(
+        gaussians[0],
+        maximums[0]
+    )
+    result[:width, :height] = keypoints[:width,:height]
+    
+    for i in range(1, len(gaussians)):
+        
+        scale_resize = np.resize(gaussians[i], (rimg.shape[1], rimg.shape[0]), interpolation = cv2.INTER_NEAREST)
+        
+        #scale_resize[:, :]
+        
+        
+        #keypoints = drawKeypoints(
+        #    ,
+        #    maximums
+        #)
+    
     
     return result
 
-img = cv2.imread("./imagenes/yosemite7.jpg", cv2.IMREAD_GRAYSCALE)
+#./imagenes/yosemite7.jpg
+img = cv2.imread("../images/plane.bmp", cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow("Hybrid Image", ejercicio1(img))
+cv2.imshow("Hybrid Image", ejercicio1(img, 0.9))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -175,7 +231,7 @@ if len(scales) > 1:
         # Get maximum from top layer only
         max_top = np.amax(fHMs[1][cy - 1 : cy + 2, cx - 1 : cx + 2])
         if maximum[2] > max_top:
-            interestpoints.append(cv2.KeyPoint(cx, cy, maximum[3], 0))
+            interestpoints.append()
     
     #interestpoints.append(interestbottom)
     
