8aa7284a9539e5217b4e2132b61534e6618308fa
Lukas Haring <lukasharing@hotmail.com>
2020-03-09 12:14:42 +0100

    VC (Las project) + MH Almost Finish

--- MH/Práctica 1/main.py
+++ MH/Práctica 1/main.py
@@ -5,6 +5,8 @@ Created on Sat Feb 29 02:39:33 2020
 @author: Lukas Häring García
 """
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import spatial
 
 import random
 
@@ -12,7 +14,7 @@ import random
 # Rand : 3 classes
 # Ecoli : 8 classes
 
-name = "rand"
+name = "iris"
 
 # Load Dat File With Numpy
 path_dat = "./dataset/"+ name +"_set.dat"
@@ -41,60 +43,163 @@ def general_deviation(C):
 #def infeasibility(C):
 #    np.sum([np.sum() for ])
 
+def violate_restriction(i, assigned, ri):
+    violations = 0
+    for (j, restriction) in enumerate(ri):
+        if assigned[j] >= 0 and i != j:
+            if restriction == 1:
+                violations += assigned[j] != assigned[i]
+            elif restriction == -1:
+                violations += assigned[j] == assigned[i]
+    
+    return violations
 
-def violate_restriction(i, clusters, restrictions):
+def lambda_factor(X, R):
+    # Get Distance Matrix
+    dist_mat = spatial.distance_matrix(X, X)
+    # Get indices of the furthest points
+    i, j = np.unravel_index(dist_mat.argmax(), dist_mat.shape)
+    # Find Number of restrictions
+    num_restrictions =  (np.sum(np.absolute(R)) - len(X)) / 2
+    # Return D / R
+    return distance(X[i], X[j]) / num_restrictions
+
+def infeasability(X, A, R):
+    result = 0.
+    for i in range(len(X)):
+        result += violate_restriction(i, A, R[i])
     
-    print()
+    return result
+
+def f(X, C, A, R):
+    return general_deviation(C) + lambda_factor(X, R) * infeasability(X, A, R)
+
+def plot2d(X, assigned, mis, restrictions):
+    fig, ax = plt.subplots()
     
-    return False
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple']
     
+    for i in range(len(X)):
+        for r in range(i, len(X)):
+            if restrictions[i][r] == -1:
+                ax.plot(
+                    [X[i][0], X[r][0]],
+                    [X[i][1], X[r][1]],
+                    "r--",
+                    linewidth = 1,
+                    alpha = 0.1
+                )
+    
+    for i in range(len(X)):
+        ax.scatter(
+            X[i][0], 
+            X[i][1],
+            c = colors[assigned[i]],
+            s = 10.
+        )
+    
+    centroids_x, centroids_y = map(list, zip(*mis))
+    for i in range(len(mis)):
+        ax.scatter(
+            centroids_x[i], 
+            centroids_y[i],
+            c = colors[i],
+            s = 50.,
+            edgecolors = [0, 0, 0]
+        )
+    
+    plt.show()
 
 def COPKM(X, R, k):
 
-    seed = 30
+    seed = 10
     
     random.seed(seed)
     np.random.seed(seed)
     
-    RSI = random.sample(range(len(X)), len(X))
-    mis = np.random.rand(k, dataset.shape[1])
+    size = len(X)
+    RSI = random.sample(range(size), size)
     
+    # Initizalization mis
+    min_val = np.amin(X, axis = 0)
+    max_val = np.amax(X, axis = 0)
+    
+    # Scale Matrix (Diagonal Matrix)
+    dif_val = np.diag(np.subtract(max_val, min_val))
+    
+    # M(n, m) random and transposed
+    rand_mtx = np.random.rand(dataset.shape[1], k)
+    
+    print(rand_mtx)
+    print(dif_val)
+    # R * Dif + Min 
+    mis = (
+        np.transpose(
+            np.matmul(
+                dif_val,
+                rand_mtx
+            )
+        ) #  R * Dif
+        + min_val
+    )
+    
+    last_assigned = np.full((1, size), -1)
     while True:
-        
-        has_change = False
-    
+        assigned = np.full((1, size), -1)
         clusters = [[] for i in range(k)]
         
-        print(mis)
         for i in RSI:
             xi = X[i]
-            # Could be optimized using an Object {distance, index}
-            nmmis = range(len(mis)) # Create Index Array
-            dsmis = [distance(mi, xi) for mi in mis] # Create Distance Array
+            # Create Index Array
+            nmmis = list(np.arange(len(mis)))
+            
+            # Create Distance Array
+            dsmis = [distance(mi, xi) for mi in mis]
+            
             # Find Array index of the minimun distance of a set of cluster
             idxmin = dsmis.index(min(dsmis))
+            
+            # Assign Element i to that cluster
+            assigned[0][i] = nmmis[idxmin]
+            
             # Check if the element in that cluster satisfies the restrictions
-            while len(dsmis) > 0 and violate_restriction(idxmin, clusters, R):
+            while violate_restriction(i, assigned[0], R[i]) > 0:
                 dsmis.pop(idxmin)
                 nmmis.pop(idxmin)
+                if len(dsmis) == 0: break
                 idxmin = dsmis.index(min(dsmis))
+                assigned[0][i] = nmmis[idxmin]
             
+            # Check the violation of the change
             if len(dsmis) == 0:
                 return []
             else:
                 clusters[nmmis[idxmin]].append(xi)
-            # Asignar xi a su cluster más cercano cj sin violar ninguna restricción
         
+        # Update Centroids
         for i in range(k):
             if len(clusters[i]) > 0:
                 mis[i] = group_centroid(clusters[i])
         
-        if not(has_change):
+        if (last_assigned[0] == assigned[0]).all():
+            #plot2d(X, assigned[0], mis, R)
             return clusters
+        
+        last_assigned = assigned
+    
+
+def LOCAL(X, R, k):
+    
+    
+    
+    f(X, C, A, R)
+    
+    
 
 clusters = COPKM(dataset, constrictions, 3)
-print(general_deviation(clusters))
 
-
-
+if(len(clusters) == 0):
+    print("No solution found")
+else:
+    print("Deviation = {}".format(general_deviation(clusters)))
 
