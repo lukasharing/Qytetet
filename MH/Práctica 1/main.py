be14908bdaecdd9b8a591ef6119652797e63222b
Lukas Haring <lukasharing@hotmail.com>
2020-03-23 22:44:08 +0100

    MH Final P1

--- MH/Práctica 1/main.py
+++ MH/Práctica 1/main.py
@@ -12,21 +12,6 @@ import random
 
 import time
 
-# Iris : 3 classes
-# Rand : 3 classes
-# Ecoli : 8 classes
-
-name = "ecoli"
-k = 8
-
-# Load Dat File With Numpy
-path_dat = "./dataset/"+ name +"_set.dat"
-dataset = np.loadtxt(path_dat, dtype='f', delimiter=',')
-
-# Load Constrictions File With Numpy
-path_const = "./dataset/"+ name +"_set_const_10.const"
-constrictions = np.loadtxt(path_const, dtype='f', delimiter=',')
-
 # Centroid Function
 def group_centroid(ci):
     return np.transpose([np.sum(xj) for xj in np.transpose(ci)]) / len(ci)
@@ -36,7 +21,6 @@ def distance(v1, v2):
     
 def distance_intracluster(ci):
     if len(ci) == 0: return 0.0
-    
     centroid = group_centroid(ci)
     return np.sum([distance(xj, centroid) for xj in ci]) / len(ci)
 
@@ -55,49 +39,55 @@ def lambda_factor(X, R):
     return distance(X[i], X[j]) / num_restrictions
 
 # Este código está explicado en la memoria
-def infeasability(X, A, p_r, n_r):
+def infeasibility(X, A, p_r, n_r):
     
     A_scale = np.diag(A + 1)
     A_scalei = np.diag([1/(a + 1) for a in A])
     A_scalei = np.where(np.isposinf(A_scalei), 0, A_scalei)
     
+    # Mnn (M'nn Pr)^t
     P_D = np.matmul(
         A_scalei,
         np.transpose(np.matmul(A_scale, p_r))
     )
     
+    # Mnn (M'nn Nr)^t
     N_D = np.matmul(
         A_scalei,
         np.transpose(np.matmul(A_scale, n_r))
     )
     
+    # Sum c Pr / c != 1,0
     positive_violation = np.count_nonzero(np.where(P_D == 1, 0, P_D))
+    # Sum c Nr / c = 1
     negative_violation = np.count_nonzero(np.where(N_D != 1, 0, N_D))
     
     return positive_violation + negative_violation
 
+# Separa El vector de asignación
 def split_to_clusters(X, A, k):
     clusters = [[] for i in range(k)]
     [clusters[v].append(X[i]) for i, v in enumerate(A)]
     return clusters
 
 def f(X, A, p_r, n_r, k, flambda):
-    return general_deviation(X, A, k) + flambda * infeasability(X, A, p_r, n_r)
+    return general_deviation(X, A, k) + flambda * infeasibility(X, A, p_r, n_r)
 
-def plot2d(X, assigned, mis, restrictions):
+def plot2d(X, assigned, mis, negative_r, color = "red"):
     fig, ax = plt.subplots()
     
     colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple']
     
     for i in range(len(X)):
         for r in range(i, len(X)):
-            if restrictions[i][r] == -1:
+            if negative_r[i][r] == 1:
                 ax.plot(
                     [X[i][0], X[r][0]],
                     [X[i][1], X[r][1]],
                     "r--",
                     linewidth = 1,
-                    alpha = 0.1
+                    alpha = 0.1,
+                    c = color
                 )
     
     for i in range(len(X)):
@@ -130,7 +120,6 @@ def random_centroids(X, k):
     
     # M(n, m) random and transposed
     rand_mtx = np.random.rand(X.shape[1], k)
-    
     # R * Dif + Min 
     return (
         np.transpose(
@@ -148,38 +137,52 @@ def has_one(assigned_result, k):
             return False
     return True
 
-def COPKM(X, R, k, seed = 11):
+def generate_neighbourhood(parent, n, k):
+    neighbourhood = []
+    for i in range(n):
+        pivot = parent[i]
+        for j in range(0, pivot):
+            parent[i] = j
+            if has_one(parent, k):
+                neighbourhood.append([i, j])
+            parent[i] = pivot
+            
+        for j in range(pivot + 1, k):
+            parent[i] = j
+            if has_one(parent, k):
+                neighbourhood.append([i, j])
+            parent[i] = pivot
+        
+    return np.random.permutation(neighbourhood)
+
+def COPKM(X, positive_r, negative_r, k, flambda = 0.0, seed = 10):
     
     random.seed(seed)
     np.random.seed(seed)
     
     size = len(X)
+    # Return I could have used np.random.permutation...
     RSI = random.sample(range(size), size)
-    
-    # Matrix
-    positive_r = R - np.identity(R.shape[0])
-    positive_r[:][:] = 0.5*positive_r[:][:]*(positive_r[:][:] + 1)
-    negative_r = R - np.identity(R.shape[0])
-    negative_r[:][:] = 0.5*negative_r[:][:]*(negative_r[:][:] - 1)
-    
+
     mis = random_centroids(X, k)
-    
+
     last_assigned = np.repeat(-1, size)
     while True:
-
+        # Generate Assign Vector with -1
         assigned = np.repeat(-1, size)
         for i in RSI:
             
             xi = X[i]
+            # Calculate distance row
             dsmis = [distance(mi, xi) for mi in mis]
             
             assigned[i] = best_cj = 0
-            min_dinfeasability = infeasability(X, assigned, positive_r, negative_r)
+            min_dinfeasability = infeasibility(X, assigned, positive_r, negative_r)
             min_distance = dsmis[0]
             
             for cj in range(1, k):
                 assigned[i] = cj
-                cinfeasability = infeasability(X, assigned, positive_r, negative_r)
+                cinfeasability = infeasibility(X, assigned, positive_r, negative_r)
                 
                 # Lowest Infeasability and proximal to distance
                 if cinfeasability < min_dinfeasability and dsmis[cj] < min_distance:
@@ -197,72 +200,221 @@ def COPKM(X, R, k, seed = 11):
                 mis[i] = group_centroid(clusters[i])
         
         if (last_assigned == assigned).all():
-            #plot2d(X, assigned, mis, R)
             return assigned
         
         last_assigned = assigned
-    
 
-EPSILON = 0.0001
-def LOCAL(X, R, k, seed = 10):
+def BL_PM(X, positive_r, negative_r, k, flambda = 0.0, seed = 10):
     
-    # Calculate Lambda Factor (Constant in the whole process)
-    flambda = lambda_factor(X, R)
-    
-    #random.seed(seed)
-    #np.random.seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
     
     size = len(X)
     
-    # Matrix
-    positive_r = R - np.identity(R.shape[0])
-    positive_r[:][:] = 0.5*positive_r[:][:]*(positive_r[:][:] + 1)
-    negative_r = R - np.identity(R.shape[0])
-    negative_r[:][:] = 0.5*negative_r[:][:]*(negative_r[:][:] - 1)
-    
     # Initialization
     assigned_result = np.random.randint(k, size = size)
+    # Sure that the elements have at least one cluster assign
+    while not(has_one(assigned_result, k)):
+        assigned_result = np.random.randint(k, size = size)
+    
     min_f = f(X, assigned_result, positive_r, negative_r, k, flambda)
     
-    for i in range(10000):
+    total_evaluation_f = 0
+    max_f_evaluation = 100000
+    found_better = True
+    while total_evaluation_f < max_f_evaluation and found_better:
         
-        # Check that all cluster have at least one element
-        while True:
-            mutate_idx = np.random.randint(size)
-            last_cluster = assigned_result[mutate_idx]
-            rand_cluster = np.random.permutation([i for i in range(k) if i != last_cluster])[0]
+        # Generate Neighbourhood
+        neighbourhood = generate_neighbourhood(assigned_result, size, k)
+        
+        found_better = False
+        for idx, cluster in neighbourhood:
+            old_neighbour = assigned_result[idx]
+            assigned_result[idx] = cluster
+            current_f = f(X, assigned_result, positive_r, negative_r, k, flambda)
             
-            assigned_result[mutate_idx] = rand_cluster
-            if has_one(assigned_result, k): break
-            else: assigned_result[mutate_idx] = last_cluster
+            if current_f < min_f:
+                min_f = current_f
+                found_better = True
+                break;
+            else: assigned_result[idx] = old_neighbour
+            
+            total_evaluation_f += 1
+            if total_evaluation_f >= max_f_evaluation: break
         
-        current_f = f(X, assigned_result, positive_r, negative_r, k, flambda)
-        
-        if current_f < min_f:
-            min_f = current_f
-        else:
-            assigned_result[mutate_idx] = last_cluster
-            # If the value is too small, then just break it
-            """if abs(current_f - min_f) < EPSILON:
-                print(current_f, min_f)
-                print("Break in {} iterations".format(i))
-                break"""
-        
-    #mis = [group_centroid(c) for c in split_to_clusters(X, assigned_result, k)]
-    #plot2d(X, assigned_result, mis, R)
+    if not(found_better):
+        print("Found solution in {} iterations!!".format(total_evaluation_f))
+    elif total_evaluation_f >= max_f_evaluation:
+        print("Exceeded number maximum of iterations!!")
     
     return assigned_result
 
-start_time = time.time()
-clusters = COPKM(dataset, constrictions, k)
-print("--- %s s ---" % round(time.time() - start_time, 2))
-print("Deviation = {}".format(round(general_deviation(dataset, clusters, k), 2)))
+
+def BL_MV(X, positive_r, negative_r, k, flambda = 0.0, seed = 10):
+    
+    random.seed(seed)
+    np.random.seed(seed)
+    
+    size = len(X)
+    
+    # Initialization
+    assigned_result = np.random.randint(k, size = size)
+    
+    # Sure that the elements have at least one cluster assign
+    while not(has_one(assigned_result, k)):
+        assigned_result = np.random.randint(k, size = size)
+    
+    min_f = f(X, assigned_result, positive_r, negative_r, k, flambda)
+    
+    total_evaluation_f = 0
+    max_f_evaluation = 1000000
+    found_better = True
+    while total_evaluation_f < max_f_evaluation and found_better:
+        
+        # Generate Neighbourhood
+        neighbourhood = generate_neighbourhood(assigned_result, size, k)
+        
+        found_better = False
+        best_idx = -1
+        best_cluster = 0
+        
+        for idx, cluster in neighbourhood:
+            # Change Solution State
+            old_neighbour = assigned_result[idx]
+            assigned_result[idx] = cluster
+            
+            current_f = f(X, assigned_result, positive_r, negative_r, k, flambda)
+            if current_f < min_f:
+                found_better = True
+                min_f = current_f
+                best_idx = idx
+                best_cluster = cluster
+            
+            # Return to last State
+            assigned_result[idx] = old_neighbour
+            
+            total_evaluation_f += 1
+            if total_evaluation_f >= max_f_evaluation: break
+        
+        if found_better:
+            assigned_result[best_idx] = best_cluster
+    
+    if not(found_better):
+        print("Found solution in {} iterations!!".format(total_evaluation_f))
+    elif total_evaluation_f >= max_f_evaluation:
+        print("Exceeded number maximum of iterations!!")
+    
+    return assigned_result
 
 
-start_time = time.time()
-clusters = LOCAL(dataset, constrictions, k)
-print("--- %s s ---" % round(time.time() - start_time, 2))
-print("Deviation = {}".format(round(general_deviation(dataset, clusters, k), 2)))
+def algoritm(name, f, seeds, X, R, k):
+    print("######### {} #########".format(name))
+    mean_time = 0.
+    mean_deviation = 0.
+    mean_tasa_inf = 0.
+    mean_agregado = 0.
+    
+    # Lambda
+    # Calculate Lambda Factor (Constant in the whole process)
+    flambda = lambda_factor(X, R)
+    
+    # Matrix
+    positive_r = R - np.identity(R.shape[0])
+    # c'ij= cij * (cij + 1)*1/2
+    positive_r[:][:] = 0.5*positive_r[:][:]*(positive_r[:][:] + 1)
+    negative_r = R - np.identity(R.shape[0])
+    # c'ij= cij * (cij - 1)*1/2
+    negative_r[:][:] = 0.5*negative_r[:][:]*(negative_r[:][:] - 1)
+    
+    for (i, seed) in enumerate(seeds):
+        start_time = time.time()
+        clusters = f(X, positive_r, negative_r, k, flambda, seed)
+        # Time
+        dt = time.time() - start_time
+        mean_time += dt
+        # General Deviation
+        deviation = general_deviation(X, clusters, k)
+        mean_deviation += deviation
+        # Infeasability
+        tasa_inf = infeasibility(X, clusters, positive_r, negative_r)
+        mean_tasa_inf += tasa_inf
+        # (Agregado) f Function: f = C + F * lambda
+        tasa_agregado = tasa_inf * flambda + deviation
+        mean_agregado += tasa_agregado
+        
+        print("\n######### Iteration {} in {} s #########".format(i + 1, round(dt, 2)))
+        print("\t1. Deviation = {}".format(round(deviation, 2)))
+        print("\t2. Infeasibility = {}".format(round(tasa_inf, 2)))
+        print("\t3. Agregado = {}".format(round(tasa_agregado, 2)))
+        
+        if False:
+            print("\n#### GRAPHS ####")
+            mis = [group_centroid(c) for c in split_to_clusters(X, clusters, k)]
+            #print("\t1. Not Link")
+            plot2d(X, clusters, mis, negative_r, "red")
+            #print("\t2. Must Link")
+            #plot2d(X, clusters, mis, positive_r, "blue")
+    
+    print("\n\n######### MEAN #########")
+    print("\t1. Mean time: {} ".format(round(mean_time / len(seeds), 2)))
+    print("\t2. Mean deviation: {}".format(round(mean_deviation / len(seeds), 2)))
+    print("\t3. Mean infeasability: {}".format(round(mean_tasa_inf / len(seeds), 2)))
+    print("\t4. Mean agregado: {}".format(round(mean_agregado / len(seeds), 2)))
 
+def main(name, n_constrictions, k):
+    
+    print("#########** {} **#########".format(name))
+    # Load Dat File With Numpy
+    path_dat = "./dataset/{}_set.dat".format(name)
+    dataset = np.loadtxt(path_dat, dtype='f', delimiter=',')
+    
+    # Load Constrictions File With Numpy
+    path_const = "./dataset/{}_set_const_{}.const".format(name, n_constrictions)
+    constrictions = np.loadtxt(path_const, dtype='f', delimiter=',')
+    
+    seeds = [10, 11, 12, 13, 14]
+    algoritm(
+        "GREEDY",
+        COPKM,
+        seeds,
+        dataset,
+        constrictions,
+        k
+    )
+    
+    algoritm(
+        "Local Search Best First",
+        BL_PM,
+        seeds,
+        dataset,
+        constrictions,
+        k
+    )
+    
+    algoritm(
+        "Local Search Best Neighbour",
+        BL_MV,
+        seeds,
+        dataset,
+        constrictions,
+        k
+    )
+    
+    
+print("#############################################")
+main("rand", 10, 3)
+print("#############################################")
+main("iris", 10, 3)
+print("#############################################")
 
-
+main("ecoli", 10, 8)
+print("#############################################")
+main("newthyroid", 10, 3)
+"""print("#############################################")
+main("rand", 20, 3)
+print("#############################################")
+main("iris", 20, 3)
+print("#############################################")
+main("ecoli", 20, 8)
+print("#############################################")
+main("newthyroid", 20, 3)"""
